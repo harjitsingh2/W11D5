db5f03294e8defa6313a10b35e48bf3e1924f4bb
harjitsingh3 <singh.harjit09@gmail.com>
2023-05-19 15:05:53 -0400

    Create mouseEnter and mouseLeave handlers and listeners

--- react-redux-pet-rating/frontend/src/components/PawsRatingInput.js
+++ react-redux-pet-rating/frontend/src/components/PawsRatingInput.js
@@ -1,4 +1,16 @@
+import {useState} from 'react';
+
 const PawsRatingInput = ({ rating, disabled, onChange }) => { 
+  
+  const [activeRating, setActiveRating] = useState(rating);
+
+  const handleMouseEnter = (value) => {
+    setActiveRating(value);
+  }
+
+  const handleMouseLeave = (rating) => {
+    setActiveRating(rating);
+  }
   
   return (
     <>
@@ -10,19 +22,19 @@ const PawsRatingInput = ({ rating, disabled, onChange }) => {
     />
     
     <div className="rating-input">
-      <div className={rating >= 1 ? "filled" : "empty"}>
+      <div className={activeRating >= 1 ? "filled" : "empty"} onMouseEnter={() => handleMouseEnter(1)} onMouseLeave={() => handleMouseLeave(rating)}>
         <i className="fa fa-paw"></i>
       </div>
-      <div className={rating >= 2 ? "filled" : "empty"} >
+      <div className={activeRating >= 2 ? "filled" : "empty"} onMouseEnter={() => handleMouseEnter(2)} onMouseLeave={() => handleMouseLeave(rating)}>
         <i className="fa fa-paw"></i>
       </div>
-      <div className={rating >= 3 ? "filled" : "empty"} >
+      <div className={activeRating >= 3 ? "filled" : "empty"} onMouseEnter={() => handleMouseEnter(3)} onMouseLeave={() => handleMouseLeave(rating)}>
         <i className="fa fa-paw"></i>
       </div>
-      <div className={rating >= 4 ? "filled" : "empty"} >
+      <div className={activeRating >= 4 ? "filled" : "empty"} onMouseEnter={() => handleMouseEnter(4)} onMouseLeave={() => handleMouseLeave(rating)}>
         <i className="fa fa-paw"></i>
       </div>
-      <div className={rating === 5 ? "filled" : "empty"} >
+      <div className={activeRating === 5 ? "filled" : "empty"} onMouseEnter={() => handleMouseEnter(5)} onMouseLeave={() => handleMouseLeave(rating)}>
         <i className="fa fa-paw"></i>
       </div>
     </div>
